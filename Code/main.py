f4f19f87073232420574ce9f5358f002bf357a6b
Max Hubmann <maxhubmann@outlook.com>
2024-09-19 13:12:10 +0200

    adaption of run class to amke it more smooth :D

--- Code/main.py
+++ Code/main.py
@@ -6,22 +6,42 @@ import time
 import cProfile
 import pstats
 import pandas as pd
-
-
-main_tasks = True
-
-if main_tasks:
-
-    instances = ['7_2_1']#, '7_5_1', '7_5_2', '7_8_1', '7_8_2']#, '7_10_1', '7_10_2']
-else:
-    instances = ['7_2_1']#, '7_8_1', '7_10_1']
+import sys
 
 
 print('______________________________________________________________________')
 
-runtimes = dict()
+results = []
+
+# Define your instance pairs
+all_instance_pairs = [
+    ['7_2_1', '7_2_2'],
+    ['7_5_1', '7_5_2'],
+    ['7_8_1', '7_8_2'],
+    ['7_2_1', '7_5_1'],
+    ['7_8_1']
+]
 
 def main():
+    if len(sys.argv) > 1:
+        # Accept a pair index argument from the command line
+        try:
+            pair_index = int(sys.argv[1])
+            if 0 <= pair_index <= len(all_instance_pairs) - 1:
+                print(f"Running pair {pair_index}...")
+                instances = all_instance_pairs[pair_index]
+                main_tasks = True
+                if pair_index > 2:
+                    main_tasks = False
+            else:
+                print(f"Please provide a pair index between 1 and {len(all_instance_pairs)}.")
+        except ValueError:
+            print("Invalid pair index. Please provide a number.")
+    else:
+        print(f"No pair index provided. Running the first pair by default.")
+        instances = all_instance_pairs[0]
+        main
+
 
     for i in instances:
         print(Path.cwd().parent) 
@@ -69,9 +89,9 @@ def main():
         SA_LS = SimulatedAnnealingLocalSearch(
             inputData=data,
             start_temperature = 1000,
-            min_temperature = 1e-40,
-            temp_decrease_factor=0.99,
-            maxRunTime=60*60*4,
+            min_temperature = 1e-20,
+            temp_decrease_factor=0.95,
+            maxRunTime=7200,
             neighborhoodTypesDelta=['SwapIntraRoute','TwoEdgeExchange','SwapInterRoute','ReplaceDelta'],
             neighborhoodTypesProfit= ['Insert','ReplaceProfit']
         )
@@ -113,60 +133,86 @@ def main():
         )
 
 
-        solver.RunAlgorithm(
-            numberParameterCombination=1,
-            main_tasks=True,
-            algorithm = SAILS_algorithm
+        iterations = solver.RunAlgorithm(
+            numberParameterCombination=3,
+            main_tasks=main_tasks,
+            algorithm = SA_LS
         )
 
 
         #print(f'Anzahl Iterationen: {iteration}')
 
         # Define the directory and file name
-        output_directory = Path.cwd().parent / "Data" / "Debug"
+        if pair_index <= 2: 
+            output_directory = Path.cwd().parent / "Data" / "SA_LS"
+            csv_name = f"sa_ls_finals_results_{pair_index}.csv"
+        else: 
+            output_directory = Path.cwd().parent / "Data" / "SA_LS_Flexi"
+            csv_name = f"sa_ls_finals_results_flexi_{pair_index}.csv"
 
-        solver.SolutionPool.GetHighestProfitSolution().WriteSolToJson(output_directory, data, True)
+        solver.SolutionPool.GetHighestProfitSolution()
 
-        #Algorithm
-        '''
-        solver.RunAlgorithm(
-            numberParameterCombination=1,
-            main_tasks=True,
-            algorithm = ILS
+        highest_profit_solution = solver.SolutionPool.GetHighestProfitSolution()
+        highest_profit_solution.WriteSolToJson(output_directory, data, main_tasks=main_tasks)
+
+        total_profit = highest_profit_solution.TotalProfit
+        waiting_time = highest_profit_solution.WaitingTime
+        total_tasks = highest_profit_solution.TotalTasks
+
+        results.append({
+            'Instance': i,
+            'TotalProfit': total_profit,
+            'WaitingTime': waiting_time,
+            'TotalTasks': total_tasks,
+            'RunTime' : round(solver.RunTime,4),
+            'Iterations' : iterations
+        })
+
+    df = pd.DataFrame(results)
+    df.to_csv(csv_name, index=False)
+
+    #Algorithm
+    '''
+    solver.RunAlgorithm(
+        numberParameterCombination=1,
+        main_tasks=True,
+        algorithm = ILS
+    )
+
+    '''
+    #Iterated Local Search
+    '''
+    solver.RunIteratedLocalSearch(
+        numberParameterCombination=1,
+        main_tasks=True,
+        algorithm_LS=neighborhoodLocalSearch,
+        algorithm_ILS=Adaptive_ILS
+    )
+    '''
+    #Local Search
+    '''
+    solver.RunAlgorithm(
+        numberParameterCombination= 3, 
+        main_tasks= main_tasks,
+        algorithm= neighborhoodLocalSearch)
+    '''
+
+    #Construction Heuristic
+    '''
+    solver.ConstructionPhase(
+        numberParameterCombination= 3, 
+        main_tasks= main_tasks,
         )
+    
+    '''
+if __name__ == "__main__":
+    main()
 
-        '''
-        #Iterated Local Search
-        '''
-        solver.RunIteratedLocalSearch(
-            numberParameterCombination=1,
-            main_tasks=True,
-            algorithm_LS=neighborhoodLocalSearch,
-            algorithm_ILS=Adaptive_ILS
-        )
-        '''
-        #Local Search
-        '''
-        solver.RunAlgorithm(
-            numberParameterCombination= 3, 
-            main_tasks= main_tasks,
-            algorithm= neighborhoodLocalSearch)
-        '''
-
-        #Construction Heuristic
-        '''
-        solver.ConstructionPhase(
-            numberParameterCombination= 3, 
-            main_tasks= main_tasks,
-            )
-        
-        '''
-
-
-
+'''
 # Profile the main function
 if __name__ == '__main__':
     cProfile.run('main()', 'profiling_results.prof')
     p = pstats.Stats('profiling_results.prof')
     p.sort_stats('cumtime').print_stats(80)  # Sort by cumulative time and show the top 10 results
+'''
 
